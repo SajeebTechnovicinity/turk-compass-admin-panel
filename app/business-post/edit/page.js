3b89caf9d73007b0985c9765c3ffae0a09f63da1
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-06-07 16:23:11 +0600

    sss

--- app/business-post/edit/page.js
+++ app/business-post/edit/page.js
@@ -637,7 +637,7 @@ export default function Form() {
                                 </div>
                                 <div className="form-group row">
                                     <label className="col-md-3 col-from-label">
-                                        Is Exempt
+                                        Exempt
                                     </label>
                                     <div className='col-md-8'>
                                         <select 
